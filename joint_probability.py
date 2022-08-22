5fda5ab3bec66a9d27956bf9769992206dbdd80a
khassibi <mekimia@gmail.com>
2022-08-22 11:16:20 -0700

    tried some probability things

--- joint_probability.py
+++ joint_probability.py
@@ -1,13 +1,28 @@
+import wandb
+wandb.login()
+WANDB_API_KEY = '78544c6ed5f52873b1588acd09ead571942d7dfd'
+
 import numpy as np
 import scipy.io
 import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+
 import seaborn as sns
+import pandas as pd
 
+import math
 
-# mat = scipy.io.loadmat('//central/groups/tensorlab/khassibi/fourier_neural_operator/data/planes.mat')
+import operator
+from functools import reduce
+from functools import partial
 
-# print(mat['P_plane'])
+from timeit import default_timer
+from utilities3 import *
+
+from Adam import Adam
 
 TRAIN_PATH = '/central/groups/tensorlab/khassibi/fourier_neural_operator/data/planes.mat'
 TEST_PATH = '/central/groups/tensorlab/khassibi/fourier_neural_operator/data/planes.mat'
@@ -15,7 +30,7 @@ TEST_PATH = '/central/groups/tensorlab/khassibi/fourier_neural_operator/data/pla
 ntrain = 3000
 ntest = 1000
 
-r = 1
+r = 8
 s1 = 768//r
 s2 = 288//r
 
@@ -24,27 +39,14 @@ training_idx = idx[:ntrain]
 testing_idx = idx[-ntest:]
 
 reader = MatReader(TRAIN_PATH)
-x_train = reader.read_field('P_plane').permute(2,0,1)[training_idx][:,::r,::r][:,:s1,:s2]
-y_train = reader.read_field('V_plane').permute(2,0,1)[training_idx][:,::r,::r][:,:s1,:s2]
+y_train = reader.read_field('V_plane')
+y_train = y_train.permute(2,0,1)[training_idx][:,::r,::r][:,:s1,:s2]
 
-x_normalizer = UnitGaussianNormalizer(x_train)
-x_train = x_normalizer.encode(x_train)
-x_test = x_normalizer.encode(x_test)
+reader.load_file(TEST_PATH)
+y_test = reader.read_field('V_plane').permute(2,0,1)[testing_idx][:,::r,::r][:,:s1,:s2]
 
 y_normalizer = UnitGaussianNormalizer(y_train)
 y_train = y_normalizer.encode(y_train)
-
-x_train = x_train.reshape(ntrain,s1,s2,1)
-x_test = x_test.reshape(ntest,s1,s2,1)
-
-mean_train_pressure = []
-for i in range(len(x_train)):
-    sum = 0
-    for j in range(len(x_train[i])):
-        for k in range(len(x_train[i][j])):
-            sum += x_train[i][j][k]
-    sum /= len(x_train[i]) * len(x_train[i][j])
-    mean_train_pressure.append(sum)
 
 mean_train_velocity = []
 for i in range(len(y_train)):
@@ -55,15 +57,6 @@ for i in range(len(y_train)):
     sum /= len(y_train[i]) * len(y_train[i][j])
     mean_train_velocity.append(sum)
 
-mean_test_pressure = []
-for i in range(len(x_test)):
-    sum = 0
-    for j in range(len(x_test[i])):
-        for k in range(len(x_test[i][j])):
-            sum += x_train[i][j][k]
-    sum /= len(x_test[i]) * len(x_test[i][j])
-    mean_test_pressure.append(sum)
-
 mean_test_velocity = []
 for i in range(len(y_test)):
     sum = 0
@@ -73,11 +66,22 @@ for i in range(len(y_test)):
     sum /= len(y_test[i]) * len(y_test[i][j])
     mean_test_velocity.append(sum)
 
+# make dataset
+d = {'train': mean_train_velocity, 'test': mean_test_velocity}
+df = pd.DataFrame(data=d)
+
+wandb.init(
+    project="FNO Planes",
+    config={
+        "model": "joint probability"
+    }
+)
+
   
 # draw jointplot with
 # kde kind
-sns.jointplot(x = "id", y = "pulse",
-              kind = "kde", data = data)
+g = sns.jointplot(x = "train", y = "test", kind = "kde", data = df)
+wandb.log({f"chart_{index}": plt})
 # Show the plot
 plt.show()
   
@@ -89,3 +93,4 @@ plt.show()
 
 # plt.show()
 
+wandb.finish()
